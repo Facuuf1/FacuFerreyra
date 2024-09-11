03e1dedf8af25a41e227754b5935576c6cdf12fb
Facundo Ferreyra <Facuuaf1@gmail.com>
2024-09-11 17:04:56 -0300

    project v2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-
-
-
 function animationDots(){
     const dotContainer = document.querySelector(".dots");
     let text = "...";
@@ -16,5 +13,35 @@ function animationDots(){
     },1000); 
  
 }
-
 animationDots();
+
+const previewProject = document.querySelectorAll(".project-preview-info");
+const container = document.querySelectorAll(".project-preview");
+const btnCopy = document.querySelector(".btn-copy");
+
+previewProject.forEach(button =>{  
+    button.addEventListener("click", (e)=>{      
+        container.forEach(btn=>{
+            btn.classList.remove("active");
+        })
+    })
+    container.forEach(btn=>{
+        btn.addEventListener("click",(e)=>{
+            e.currentTarget.classList.add("active");
+        })
+    })
+})
+
+
+
+btnCopy.addEventListener("click",()=>{
+    const text = document.querySelector(".email").innerHTML;
+    
+    navigator.clipboard.writeText(text);
+    
+})
+
+
+
+
+
